4a05ad0a4f23c55ca6e2102731c81a7650b1ac28
Aman Sharma <arcaman07@Amans-MacBook-Pro.local>
2024-07-14 15:16:25 +0530

    added library functionalities

---
+++ build/lib/sketch_nn/__init__.py
@@ -0,0 +1,11 @@
+# Import the main class from the designer module
+from .designer import NeuralNetworkDesigner
+
+# Import any utility functions you want to make directly accessible
+from .utils import save_uploaded_file  # assuming you have this function in utils.py
+
+# You can also define the version of your package here
+__version__ = "0.1.5"
+
+# If you want to control what gets imported with "from sketch_nn import *"
+__all__ = ['NeuralNetworkDesigner', 'save_uploaded_file']
